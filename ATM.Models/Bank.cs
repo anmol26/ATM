8dc113c890869ebdb49633d1c68529df6b3e0d3d
Anmol Khandelwal <a.khandelwal2672001@gmail.com>
2021-10-23 12:09:54 +0530

    Commit

--- ATM.Models/Bank.cs
+++ ATM.Models/Bank.cs
@@ -6,8 +6,13 @@ namespace ATM.Models
 {
     public class Bank
     {
-        public string Name { get; } = "Alpha Bank";
-        public string Address { get; } = "Alpha Bank, Mathura(UP)";
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string Address { get; set; }
         public List<Account> Accounts { get; set; }
+
+        public readonly DateTime currentDate;
+
+
     }
 }
